67f2a0cb2c22b2cf5ccd6923851a2179a7eb135d
atafs <atafs@iscte-iul.pt>
2015-09-24 19:29:20 +0100

    first commit

---
+++ database_001_first/sql/hibernatedb_001.sql
@@ -0,0 +1,17 @@
+﻿--QUERY ALL
+SELECT *
+FROM monthly_savings;
+
+--CREATE TABLE
+CREATE TABLE monthly_savings (
+   name text,
+   saving_per_quarter integer[],
+   scheme text[][]
+);
+
+--INSERT DATA INTO TABLE
+INSERT INTO monthly_savings
+VALUES ('Manisha',
+'{20000, 14600, 23500, 13250}',
+'{{"FD", "MF"}, {"FD", "Property"}}');
+
